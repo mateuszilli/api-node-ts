df6f475bb217f95aa9609bf9798e2f33484ccc89
Mateus Zilli Polli <mateus_zillip@hotmail.com>
2021-03-09 17:59:49 -0300

    :rocket:

--- src/tests/Amount.test.ts
+++ src/tests/Amount.test.ts
@@ -2,11 +2,18 @@ import request from "supertest";
 import { app } from "../app";
 
 import createConnection from "../../database";
+import { getConnection } from "typeorm";
 
 describe("Amounts", () => {
     beforeAll(async () => {
         const connection = await createConnection();
         await connection.runMigrations();
+    });
+
+    afterAll(async () => {
+        const connection = getConnection();
+        await connection.dropDatabase();
+        await connection.close();
     });
 
     it("Should create a new amount", async () => {
